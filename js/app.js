61314b135e1fd7c0366f33de9d463b393c683177
keith wong <keith wong>
2016-06-22 12:58:32 -0700

    update 2

--- js/app.js
+++ js/app.js
@@ -2,94 +2,128 @@
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  	$('.what').click(function(){
+    	$('.overlay').fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  	$('a.close').click(function(){
+  		$('.overlay').fadeOut(1000);
   	});
 
+  	newGame();
+});
+
+// global variables
+var guessCount;
+var wonGame;
+var secretNumber;
+
+// Clear guess text section
+var clearGuess = function() {
+	$('#userGuess').val('').focus();
+}
+
+// Remove past guesses
+var removePastGuesses = function(){
+	$('ul.guessBox li').remove();
+}
+
+// Starts new game when page loads and button is clicked
+function newGame() {
+	updateFeedback('Make your guess!');
+	guessCount = 0;
+	generateNumber();
+}
+
+// Secret number generated between 1 - 100 each time new game starts
+var generateNumber = function() {
+	secretNumber = Math.floor((Math.random() * 100) + 1); 
+};
+
+
+// Display feedback
+function updateFeedback(feedback) {
+	$('#feedback').html(feedback);
+};
+
+// Feedback
+// TODO: Make secret number global
+function updateFeedbackFromGuess(guess) {
+	var diff = Math.abs(secretNumber - guess);
+	if ( secretNumber === guess ) {
+		updateFeedback('You won!');
+	}
+	else if ( diff > 50 ) {
+		updateFeedback('Ice Cold');
+	}
+	else if ( diff > 30 || diff <= 50) {
+		updateFeedback('Cold');
+	}
+	else if ( diff > 20 || diff <= 30) {
+		updateFeedback('Warm');
+	}
+	else if ( diff > 10 || diff <= 20) {
+		updateFeedback('Hot');
+	}
+	else if ( diff >= 1 || diff <= 10) {
+		updateFeedback('Very Hot');
+	}
+	else {
+		updateFeedback('Cold');
+	}
+}
+
+// track how many guesses the user has made 
+function trackGuessCount() {
+	
+}
+
+
+function updateGuessCount() {
+	guessCount++;
+	$('#count').text(guessCount);
+	//increment guessCount
+	//log out guessCount to span#count
+}
+
+
+// Supply list of numbers user has guessed
+function guessList() {
+	$('#guessList').append('<li>' + guess + '</li>');
+}
+
+// Ensure user supplies numeric input between 1 - 100
+$('form').submit(function(event){
+	event.preventDefault();
+	if (wonGame === false) {
+		guess = +$('#userGuess').val();
+		if (guess % 1 !== 0 || guess > 100 || guess < 0){
+			alert('Not a Valid Number');
+		}
+		else {
+			event.preventDefault();
+			$('#guessBox').append('<li>' + guess + '</li>');
+			clearGuess();
+			guessCount++
+			guessList();
+			updateFeedbackFromGuess(guess);
+		}
+	}
+	else {
+		updateFeedback('Start a new game.');
+	}
 });
 
 
-// starts new game when page loads
-function newGame () {
-
-}
-
-// secret number generated between 1 - 100 each time new game starts
-function generateNumber() {
-	var secretNumber = Math.floor((Math.random() * 100) + 1);
-}
-
-// feedback for each guess using div#feedback
-
-// greater than 50 = ice gold, 30 - 50 = cold, 20 - 30 = warm, 10 - 20 = hot, 1 - 10 = very hot
-
-function generateFeedback() {
-	if ( secretNumber === userInput ) {
-		winner();
-	}
-	else if ( i > 50 ) {
-		userFeedback('Ice Cold');
-	}
-	else if ( i > 30 || i <= 50) {
-		userFeedback('Cold');
-	}
-	else if ( i > 20 || i <= 30) {
-		userFeedback('Warm');
-	}
-	else if ( i > 10 || i <= 20) {
-		userFeedback('Hot');
-	}
-	else if ( i >= 1 || i <= 10) {
-		userFeedback('Very Hot');
-	}
-	else {
-		userFeedback('Cold');
-	}
-}
-
-// "Make your guess!" text each time page loads
-
-function guess () {
-
-}
-
-// track how many guesses the user has made using span#count
-
-function numberOfGuesses () {
-
-}
-
-// supply list of numbers user has guessed
-
-function numberList () {
-
-}
-
-// ensure user supplies numeric input between 1 - 100
-
-function validateInput(inputNumber) {
-	var isDecimalorNeg = inputNumber % 1 !==0;
-	if (isDecimalorNeg) {
-		displayError('Input positive whole number');
-	}
-	else if (isNaN(inputNumber)) {
-		displayError('Input is not a number');
-	}
-	else if (inputNumber === 0) {
-		displayError('Input cannot be 0!');
-	}
-	else {
-		doGuess(inputNumber);
-	}
-}
-
-function convertToNumber () {
-	return +userInput
-}
-
+// New game
+$('.new').click(function(){
+	newGame();
+	clearGuess();
+	guessCount = 0;
+	wonGame = false;
+	removePastGuesses();
+	updateGuessCount();
+	updateFeedback('Make your guess!');
+});
