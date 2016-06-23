add610e1b6a8083899c855530d7525629391932a
keith wong <keith wong>
2016-06-23 12:26:45 -0700

    finishing touches

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	/*--- Display information modal box ---*/
-  	$('.what').click(function(){
+  	$('.what').click(function() {
     	$('.overlay').fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
-  	$('a.close').click(function(){
+  	$('a.close').click(function() {
   		$('.overlay').fadeOut(1000);
   	});
 
@@ -16,64 +16,57 @@ $(document).ready(function(){
 	$('.new').click(newGame);
 		
 	// Ensure user supplies numeric input between 1 - 100
-	$('form').submit(function(event){
+	$('form').submit(function(event) {
 		event.preventDefault();
-		if (wonGame === false) {
-			guess = +$('#userGuess').val();
-			if (guess % 1 !== 0 || guess > 100 || guess < 0){
-				alert('Not a Valid Number');
-			}
-			else {
-				handleGuess(guess);
-			}
+		if (wonGame === true){
+			updateFeedback('Start a new game');
+			return;
+		}
+		guess = +$('#userGuess').val();
+		if (guess % 1 !== 0 || guess > 100 || guess < 0) {
+			alert('Not a Valid Number. Please enter a whole number 1 - 100.');
 		}
 		else {
-			updateFeedback('Start a new game.');
+			handleGuess(guess);
 		}
+
+		// if (wonGame === false) {
+		// 	guess = +$('#userGuess').val();
+		// 	if (guess % 1 !== 0 || guess > 100 || guess < 0){
+		// 		alert('Not a Valid Number');
+		// 	}
+		// 	else {
+		// 		handleGuess(guess);
+		// 	}
+		// }
+		// else {
+		// 	updateFeedback('Start a new game.');
+		// }
 	});
 
 	newGame();
 });
 
 // global variables
+var secretNumber;
 var guessCount;
 var wonGame;
-var secretNumber;
-
-// Clear guess text section
-var clearGuessInput = function() {
-	$('#userGuess').val('').focus();
-}
-
-var resetGuesses = function() {
-	guessCount = 0;
-	$('ul.guessBox li').remove();
- 	clearGuessInput();
-}
-
 
 // Starts new game when page loads and button is clicked
 function newGame() {
-	updateFeedback('Make your guess!');
 	generateNumber();
-	resetGuesses();
-	wonGame = false;
 	updateGuessCount();
-}
+	wonGame = false;
+	resetGuesses();	
+	updateFeedback('Make your guess!');
+};
 
 // Secret number generated between 1 - 100 each time new game starts
 var generateNumber = function() {
 	secretNumber = Math.floor((Math.random() * 100) + 1); 
 };
 
-
-// Display feedback
-function updateFeedback(feedback) {
-	$('#feedback').html(feedback);
-};
-
 // Feedback
-// TODO: Make secret number global
 function updateFeedbackFromGuess(guess) {
 	var diff = Math.abs(secretNumber - guess);
 	if ( secretNumber === guess ) {
@@ -97,24 +90,44 @@ function updateFeedbackFromGuess(guess) {
 	else {
 		updateFeedback('Cold');
 	}
-}
+};
 
-// track how many guesses the user has made 
-function trackGuessCount() {
-	
-}
+// Display feedback
+function updateFeedback(feedback) {
+	$('#feedback').html(feedback);
+};
 
 function handleGuess(guess) {
 	$('.guessBox').append('<li>' + guess + '</li>');
-	clearGuessInput();
 	updateGuessCount();
 	updateFeedbackFromGuess(guess);
-}
+	clearGuessInput();
+};
 
 function updateGuessCount() {
 	guessCount++;
 	$('#count').text(guessCount);
-}
+};
+
+// Clear guess text section
+var clearGuessInput = function() {
+	$('#userGuess').val('').focus();
+};
+
+var resetGuesses = function() {
+	guessCount = 0;
+	$('ul.guessBox li').remove();
+ 	clearGuessInput();
+};
+
+
+
+
+
+
+
+
+
 
 
 
